da1345471acf2099de237c5c7d44e6dc83a3ce50
Joseph-Mamian <jsmamianb@ufpso.edu.co>
2024-03-01 11:36:10 -0500

    Creacion de clase interna

--- Whenwhen/src/main/java/com/mycompany/whenwhen/Whenwhen.java
+++ Whenwhen/src/main/java/com/mycompany/whenwhen/Whenwhen.java
@@ -1,16 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.whenwhen;
 
-/**
- *
- * @author SCIS2PC12
- */
 public class Whenwhen {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Character Homero = new Character("Homero", "inspector de seguridad nuclear", 33 , "dou" );
+        Homero.greeting();
+        Character Bart = new Character("Bartolomeo", "Vago", 10 , "Ay caramba" );
+        Bart.greeting();
+    }
+    
+    static class Character{
+       String name;
+       String job;
+       int age;
+       String phrase;
+       
+       Character(String name, String job, int age, String phrase){
+        this.name = name;
+        this.job = job;
+        this.age = age;
+        this.phrase = phrase;
+       }
+       
+       
+       void greeting(){
+       System.out.println(this.phrase);
+       }
     }
 }
