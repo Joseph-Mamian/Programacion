1f26d071534ade0748c6457fc93520583501e927
Joseph-Mamian <jsmamianb@ufpso.edu.co>
2024-03-01 10:49:32 -0500

    new proyect

---
+++ Whenwhen/src/main/java/com/mycompany/whenwhen/Whenwhen.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.whenwhen;
+
+/**
+ *
+ * @author SCIS2PC12
+ */
+public class Whenwhen {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
